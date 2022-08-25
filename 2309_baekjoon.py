b5ce8dc092bc51fcb56b66704bc8ca935c1c41f5
yoonif04 <92147781+yoonif04@users.noreply.github.com>
2022-08-25 23:26:08 +0900

    Update 2309_baekjoon.py

--- 2309_baekjoon.py
+++ 2309_baekjoon.py
@@ -7,10 +7,15 @@ for _ in range(n):
 diff = sum(height) - 100
 
 # 두 키의 합이 diff만큼 되는 경우 찾아서 빼기
+flag = False
 for i in range(n-1):
     for j in range(i+1, n):
         if height[i] + height[j] == diff:
             height[i] = height[j] = 0
+            flag = True
+            break
+    if flag:
+        break
 
 # 정렬 후 출력
 for i in range(n-1):
