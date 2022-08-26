85a0af90c7d989c71c9ea9624a9f12442bf37fad
yoonif04 <92147781+yoonif04@users.noreply.github.com>
2022-08-26 17:34:10 +0900

    Update 1226_swea.py

--- 1226_swea.py
+++ 1226_swea.py
@@ -7,10 +7,11 @@ dj = [0, 0, -1, 1]
 def bfs(i, j):
     visited[i][j] = 1
     q = [(i, j)]  # 시작점 넣기
+    # graph[i][j] = 1     # 중복 방문 방지 - visited없어도됨
     while q:
         i, j = q.pop(0)
-        if i == j == 13:    # 도착점이면 함수 종료
-            return
+        if graph[i][j] == 3:    # 도착점이면 함수 종료
+            return 1
         # 4방향 탐색
         for d in range(4):
             ni, nj = i + di[d], j + dj[d]
@@ -18,6 +19,7 @@ def bfs(i, j):
             if 0 <= ni < N and 0 <= nj < N and graph[ni][nj] != 1 and not visited[ni][nj]:
                 visited[ni][nj] = 1
                 q.append((ni, nj))
+    return 0
 
 
 for _ in range(1, 11):
@@ -25,5 +27,4 @@ for _ in range(1, 11):
     N = 16  # 가로,세로 길이
     graph = [list(map(int, input())) for _ in range(N)]
     visited = [[0] * 16 for _ in range(16)]
-    bfs(1, 1)
-    print(f"#{tc} {visited[13][13]}")
+    print(f"#{tc} {bfs(1, 1)}")
