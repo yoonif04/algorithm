289d64bc134888053638288cdc6bc2798a78dd9c
yoonif04 <92147781+yoonif04@users.noreply.github.com>
2022-09-14 13:21:04 +0900

    0914

--- 22856_baekjoon.py
+++ 22856_baekjoon.py
@@ -1,53 +1,59 @@
-# 실패
+# 노드 개수가 최대 100,000개이고 유사중위순회에서 호출이 계속됨
+# setrecursionlimit() 변경
 import sys
-sys.setrecursionlimit(1000000)
-def find_root():
-    for i in range(1, N+1):
-        if par[i] == 0:
-            return i
+sys.setrecursionlimit(100000*10)
+
+
+# 중위순회
 def inorder(n):
     global last
     if n != -1:
         inorder(left[n])
-        last = n
+        last = n  # 방문할 때마다 갱신
         inorder(right[n])
 
+
+# 유사중위순회
+def similar(n):
+    global last, cnt
+    visited[n] = 1  # 방문처리
+    # 왼쪽이 존재하고, 방문하지 않았다면
+    if left[n] != -1 and not visited[left[n]]:
+        cnt += 1    # 이동횟수 증가
+        similar(left[n])    # 함수 호출
+    # 오른쪽이 존재하고, 방문하지 않았다면
+    elif right[n] != -1 and not visited[right[n]]:
+        cnt += 1    # 이동횟수 증가
+        similar(right[n])   # 함수 호출
+    # 현재 노드번호가 last에 저장된 중위순회의 마지막과 같다면 종료
+    elif n == last:
+        return
+    # 부모가 존재한다면(0이 아니라면)
+    elif par[n]:
+        cnt += 1    # 이동횟수 증가
+        similar(par[n])     # 함수 호출
+
+
+# 노드의 개수
 N = int(input())
-# 노드의 왼쪽, 오른쪽, 부모 저장 변수
-left = [0] * (N+1)
-right = [0] * (N+1)
-par = [0] * (N+1)
-# 노드의 정보 저장
+left = [0] * (N + 1)    # 왼쪽 노드 저장
+right = [0] * (N + 1)   # 오른쪽 노드 저장
+par = [0] * (N + 1)     # 부모 노드 저장
+# 정보 입력받아서 왼쪽, 오른쪽, 부모 노드 입력
 for _ in range(N):
     n, l, r = map(int, input().split())
     left[n] = l
     right[n] = r
+    # 왼쪽 존재한다면 왼쪽 노드의 부모를 n으로 저장
     if l != -1:
         par[l] = n
+    # 오른쪽 존재한다면 오른쪽 노드의 부모를 n으로 저장
     if r != -1:
         par[r] = n
 
-cnt = 0     # 이동횟수 셀 변수
-now = find_root()     # 루트노드에서 출발
-inorder(now)
-visited = [0] * (N+1)   # 방문여부 체크 변수
-visited[now] = 1
-while True:
-    # 왼쪽 존재하고, 방문 안했다면 왼쪽으로 이동
-    if left[now] != -1 and not visited[left[now]]:
-        visited[left[now]] = 1    # 방문처리
-        now = left[now]    # 왼쪽으로 이동
-        cnt += 1    # 이동횟수 늘리기
-    # 오른쪽 존재하고, 방문 안했다면 오른쪽으로 이동
-    elif right[now] != -1 and not visited[right[now]]:
-        visited[right[now]] = 1
-        now = right[now]   # 오른쪽으로 이동
-        cnt += 1    # 이동횟수 늘리기
-    # 그렇지 않고 부모 노드가 존재한다면, 부모 노드로 이동
-    elif par[now] != 0:
-        now = par[now]
-        cnt += 1
-    # 중위순회의 끝과 같다면, 종료
-    if now == last:
-        break
+last = 0  # 중위순회 시 마지막 노드 번호
+inorder(1)  # 루트는 1로 중위순회 -> last값 갱신됨
+visited = [0] * (N + 1)     # 방문여부 기록할 변수
+cnt = 0     # 이동횟수 기록할 변수
+similar(1)  # 루트 1로 유사중위순회 -> cnt값 갱신됨
 print(cnt)
